7f0353730dd39ea01b14ba6236ab8c9414405902
kvadrats <emils.kantans@gmail.com>
2016-07-06 12:32:27 +0300

    google api, oauth installed, quieckstart.rb, callback problems

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+def redirect
+  client = Signet::OAuth2::Client.new({
+    client_id: ENV.fetch('GOOGLE_API_CLIENT_ID'),
+    client_secret: ENV.fetch('GOOGLE_API_CLIENT_SECRET'),
+    authorization_uri: 'https://accounts.google.com/o/oauth2/auth',
+    scope: Google::Apis::CalendarV3::AUTH_CALENDAR_READONLY,
+    redirect_uri: url_for(:action => :callback)
+  })
+
+  redirect_to client.authorization_uri.to_s
+end
 end
